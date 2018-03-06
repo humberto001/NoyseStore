fc85796c041078bd927dee381fc29fcd25d4403a
Kelvym Miranda <kelvymm@gmail.com>
2018-03-05 22:11:14 -0300

    init maven, spring boot

---
+++ NoyseStore/src/main/java/com/noysestore/Noyse/Store/NoyseStoreApplication.java
@@ -0,0 +1,12 @@
+package com.noysestore.Noyse.Store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoyseStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoyseStoreApplication.class, args);
+	}
+}
---
+++ NoyseStore/src/test/java/com/noysestore/Noyse/Store/NoyseStoreApplicationTests.java
@@ -0,0 +1,16 @@
+package com.noysestore.Noyse.Store;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NoyseStoreApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
